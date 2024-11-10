095aac7579cb9bb9ead48c6691bc9da3d0b3744a
RobinSinghChouhan <robinsingh.02@gmail.com>
2024-11-10 22:55:02 +1100

    Added Prisma

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,71 @@
 const express = require("express");
-const app = express();
+const {PrismaClient} = require("@prisma/client")
 
-app.get("/hello",(req,res)=>{
+const app = express();
+app.use(express.json());
+const prisma = new PrismaClient();
+
+app.post("/add", (req,res)=>{
+    const title = req.body.title;
+    const desc = req.body.description;
+    const resText = insertTodo(title,desc);
     return res.json({
-        message: "Hello World"
+        message: resText
     })
 })
+
+app.get("/todos", async (req,res)=>{
+    const todos = await getAllTodos()
+    return res.json({
+        "Todos:":todos
+    })
+})
+
+app.post("/todo",async (req,res)=>{
+    const id = req.query.id;
+    console.log("ID: "+id);
+    const todo = await getTodo(id);
+    if(todo)
+    {
+        return res.json({
+            "Todo:":todo
+        }) 
+    }
+    return res.json({
+        message: "No Todos found!"
+    })
+})
+
+async function getAllTodos()
+{
+    const todos = await prisma.todos.findMany();
+    return todos;
+}
+
+async function getTodo(id)
+{
+    const todo = await prisma.todos.findUnique({
+        where: {
+            id: Number(id)
+        }
+    });
+    console.log(todo);
+    return todo;
+}
+
+async function insertTodo(title,description)
+{
+    console.log("Insert called")
+    const res = await prisma.todos.create({
+        data: {
+            title,
+            description
+        }
+    });
+    console.log(res);
+    console.log("Inserted");
+    return "Hello";
+}
 
 app.listen(3000,()=>{
     console.log("Server Running!")
